b5129e47f1be6a11ae5199d82ade48a5ed697bfb
samir <sgallegol@unal.edu.co>
2022-06-17 19:49:43 -0500

    practica adicional semana 4

---
+++ shoppingcart/src/main/java/ejercicio/semana/cuatro/collections/samir/MainCollection.java
@@ -0,0 +1,29 @@
+package ejercicio.semana.cuatro.collections.samir;
+
+public class MainCollection {
+	public static void main(String[] args) {
+		
+		Integer[] array1= {1,2,3,4,5};
+		
+		
+		CollectionCustom<Integer> array= new CollectionCustom<Integer>(array);
+		
+		System.out.println("la dimension del array es:"+ array.size());
+		array.AddFirst(7);
+		array.AddLast(23);
+		if(array.Add(40)) {
+			System.out.println("es verdadero");
+		}
+		System.out.println("valor ratirado"+ array.Remove(4));
+		array.RemoveAll();
+		System.out.println("no hay nada ene l array"+ array.Empty());
+		
+		
+	}
+	
+	
+
+	
+	
+
+}
